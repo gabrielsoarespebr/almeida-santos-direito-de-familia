be6c71384d5009e0d22da79d717cffc11993654e
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-12-10 22:50:22 -0300

    feat: Commenting unnecessary sections

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { DividerComponent } from '../../shared/components/divider/divider.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { CompetitiveEdgesComponent } from './components/competitive-edges/competitive-edges.component';
+// import { CompetitiveEdgesComponent } from './components/competitive-edges/competitive-edges.component';
 import { LegalServicesComponent } from './components/legal-services/legal-services.component';
 import { CtaComponent } from './components/cta/cta.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NewsComponent } from './components/news/news.component';
-import { WarningComponent } from './components/warning/warning.component';
+// import { NewsComponent } from './components/news/news.component';
+// import { WarningComponent } from './components/warning/warning.component';
 
 @Component({
   selector: 'app-home',
@@ -19,13 +19,13 @@ import { WarningComponent } from './components/warning/warning.component';
     DividerComponent,
     BannerComponent,
     TestimonialsComponent,
-    CompetitiveEdgesComponent,
+    // CompetitiveEdgesComponent,
     LegalServicesComponent,
     CtaComponent,
     FaqComponent,
     FooterComponent,
-    NewsComponent,
-    WarningComponent,
+    // NewsComponent,
+    // WarningComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
