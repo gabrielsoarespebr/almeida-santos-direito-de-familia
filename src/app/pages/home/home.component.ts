3c685ff029230abca9b291d561bb301a1d1dd8ba
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-12-14 22:55:14 -0300

    refactor: CTA with Google Review replaced Testimonials section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { DividerComponent } from '../../shared/components/divider/divider.component';
 import { BannerComponent } from './components/banner/banner.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+// import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 // import { CompetitiveEdgesComponent } from './components/competitive-edges/competitive-edges.component';
 import { LegalServicesComponent } from './components/legal-services/legal-services.component';
 import { CtaComponent } from './components/cta/cta.component';
@@ -18,7 +18,7 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     DividerComponent,
     BannerComponent,
-    TestimonialsComponent,
+    // TestimonialsComponent,
     // CompetitiveEdgesComponent,
     LegalServicesComponent,
     CtaComponent,
